81d142881b5407da5acb072fee52401a119a8ea4
cf181658 <cf181658@DESKTOP-D4CLIPB>
2020-04-19 13:20:05 +0200

    Complexe + Fractale

---
+++ Projet S4 (3)/Complexe.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_S4__3_
+{
+    class Complexe
+    {
+        //Variables
+        public double a; //Réel
+        public double b; //Imagniaire de a+ib
+
+        //Constructeur
+        public Complexe(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        //Méthodes
+        public void Carre()
+        {
+            double valeurtemporaire = (a * a) - (b * b);
+            b = 2.0 * a * b;
+            a = valeurtemporaire;
+        }
+        public double Norme()
+        {
+            return Math.Sqrt((a * a) + (b * b));
+        }
+        public void Addition(Complexe c)
+        {
+            a += c.a;
+            b += c.b;
+        }
+    }
+}
--- Projet S4 (3)/MyImage.cs
+++ Projet S4 (3)/MyImage.cs
@@ -498,6 +498,41 @@ namespace Projet_S4__3_
             }
             File.WriteAllBytes("newimage.bmp", newfile);
         }
+        public void Fractaleee()
+        {
+            int largeur_cadre = 400;
+            int hauteur_cadre = 400;
+            MyImage fractale = new MyImage(largeur_cadre, hauteur_cadre);
 
+            for (int i = 0; i < largeur_cadre; i++)
+            {
+                for (int j = 0; j < hauteur_cadre; j++)
+                {
+                    double a = (double)(i - (largeur_cadre / 2)) / (double)(largeur_cadre / 4);
+                    double b = (double)(j - (hauteur_cadre / 2)) / (double)(hauteur_cadre / 4);
+                    Complexe c = new Complexe(a, b);
+                    Complexe z = new Complexe(0, 0);
+                    int it = 0;
+                    int it_max = 50;
+                    do
+                    {
+                        it++;
+                        z.Carre();
+                        z.Addition(c);
+                        /*if (z.Norme() > 2.0)
+                        {
+                            break;
+                        }*/
+                    }
+                    while (it < it_max);
+                    if ((it == it_max) && (z.Norme() < 6))
+                    {
+                        fractale.Image[j, i].Rouge = 0;
+                        fractale.Image[j, i].Vert = 0;
+                        fractale.Image[j, i].Bleu = 0;
+                    }
+                }
+            }
+        }
     }
 }
